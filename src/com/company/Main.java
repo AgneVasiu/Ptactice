51a55443a685daa625ef03f6e627aa92869f70d2
AgneVasiu <agne.vasiukeviciute@stud.viko.lt>
2022-03-28 10:54:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static Logger logger = Logger.getLogger(String.valueOf(Main.class));
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+        System.out.println("Hi");
+
+
+    }
+}
